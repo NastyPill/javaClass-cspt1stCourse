1824591585d66b2aa35c4b09c311eda5ee72b391
Laptop <DJPonnYash.Vinyl@gmail.com>
2019-03-11 11:22:28 +0300

    add utility methods and edit constructors

--- src/main/java/longArithm/DecimalNumber.java
+++ src/main/java/longArithm/DecimalNumber.java
@@ -1,48 +1,81 @@
 package longArithm;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 public class DecimalNumber {
 
-    private long fractPartLength;
     private String fractionalPart;
-    private long intPartLength;
     private String integerPart;
-    private StringBuilder wholeNumber;
+    private StringBuilder number;
+
+    /*
+     *   Construct Decimal number from two parts
+     *   which are strings
+     */
 
     public DecimalNumber(String fractionalPart, String integerPart) {
         this.fractionalPart = fractionalPart;
         this.integerPart = integerPart;
-
-        wholeNumber.append(integerPart);
-        wholeNumber.append('.');
-        wholeNumber.append(fractionalPart);
+        setNumber();
     }
 
-    public DecimalNumber(long fractPartLength, long intPartLength) {
-        this.fractPartLength = fractPartLength;
-        this.intPartLength = intPartLength;
+    //  Construct Decimal number from primitive
+
+    public DecimalNumber(Number number) {
+        setParts(number);
+        setNumber();
     }
 
-    public void setNumber(Number number) {
-        if(number instanceof Long) {
+    private void setParts(Number number) {
+        if (number instanceof Long) {
             integerPart = Long.toString(number.longValue());
         }
-        if(number instanceof Integer) {
+        if (number instanceof Integer) {
             integerPart = Integer.toString(number.intValue());
         }
-        if(number instanceof Float) {
-            String[] arr = Float.toString(number.floatValue()).split(".");
+        if (number instanceof Float) {
+            String[] arr = Float.toString(number.floatValue()).split("\\.");
             integerPart = arr[0];
             fractionalPart = arr[1];
         }
-        if(number instanceof Float) {
-            String[] arr = Double.toString(number.doubleValue()).split(".");
+        if (number instanceof Double) {
+            String[] arr = Double.toString(number.doubleValue()).split("\\.");
             integerPart = arr[0];
             fractionalPart = arr[1];
         }
+    }
+
+    private void setNumber() {
+        number = new StringBuilder();
+        number.append(integerPart);
+        number.append(".");
+        number.append(fractionalPart);
     }
 
     public String getNumber() {
-        return wholeNumber.toString();
+        return number.toString();
+    }
+
+    private String reverseString(String number) {
+        return new StringBuilder(number).reverse().toString();
+    }
+
+    //array represents number like xxxx * 10^i
+
+    private int[] setPowArray(String number) {
+        int size = number.length() / 4 + 1;
+        int[] powArray = new int[size];
+        String reversedNum = reverseString(number);
+        for (int i = 0; i < size; i++) {
+            int endIndex = (i + 1) * 4;
+            if((i + 1) * 4 > number.length() - 1) {
+                endIndex = number.length() - 1;
+            }
+            powArray[0] = Integer.parseInt(reversedNum.substring(i * 4, endIndex));
+        }
+        return powArray;
     }
 
     public String sum(DecimalNumber other, int accuracy) {
@@ -66,7 +99,21 @@ public class DecimalNumber {
     }
 
     public Number getStandartType(DecimalNumber number, standartTypes type) {
-        //TODO()
-        throw new UnsupportedOperationException();
+        switch (type) {
+            case INT: {
+
+            }
+            case LONG: {
+
+            }
+            case DOUBLE: {
+
+            }
+            case FLOAT: {
+
+            }
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
---
+++ src/main/java/longArithm/MainClass.java
@@ -0,0 +1,8 @@
+package longArithm;
+
+public class MainClass {
+    public static void main(String[] args) {
+        DecimalNumber num = new DecimalNumber(123.9999);
+        System.out.println(num.getNumber());
+    }
+}
